87ca884866c2b8fa18a5f18a472e9bb5d123acd8
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-08-17 08:34:23 +0900

    [comment] add comment to exit_example.c

--- exit_example.c
+++ exit_example.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// atexitは以下のような関数プロトタイプ
+// int atexit(void (*function)(void));
+// void my_function(void);
+// 登録した関数をプロセス終了時に実行する順序は、登録時とは逆になる。
+
 void out1(void) {
     printf("out1 succeeded!\n");
 }
