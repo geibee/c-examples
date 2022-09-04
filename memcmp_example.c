bea1f035a9ce3347c6ae2b8e9b277b50e5548edd
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-09-05 08:04:47 +0900

    [comment] memcmp_example.c

--- memcmp_example.c
+++ memcmp_example.c
@@ -7,6 +7,10 @@ typedef struct {
     char* boat_name;
 } dinghy;
 
+/*
+  メモリの内容はmemcmp()で比較できる。ただし、構造体は内部にパディングを含む。
+  パディングは未初期化の不定値のため、構造体同士の比較はメンバを一つずつ比較する。
+*/
 int compare_dinghies(dinghy *a, dinghy *b) {
     int ret;
 
@@ -28,11 +32,14 @@ int compare_dinghies(dinghy *a, dinghy *b) {
     return 0;
 }
 int main() {
-    int ab, ac, ad;
+    int ab, ac, ad, test_test2, test_test3, test_test32;
     dinghy a = { 1, "test"};
     dinghy b = { 1, "testing"};
     dinghy c = { 2, "test"};
     dinghy d = { 1, "test"};
+    char* test = "test";
+    char* test2 = "test";
+    char* test3 = "testing";
 
     ab = compare_dinghies(&a, &b);
     if (!ab)
@@ -43,4 +50,16 @@ int main() {
     ad = compare_dinghies(&a, &d);
     if (!ad)
         printf("A and D are the same.\n");
+
+    test_test2 = memcmp(test, test2, sizeof(test));
+    if (test_test2)
+        printf("test and test2 are different.\n");    
+    else 
+        printf("test and test2 are the same.\n");
+        
+    test_test3 = memcmp(test, test3, sizeof(test));
+    if (test_test3)
+        printf("test and test3 are different.\n");    
+    else 
+        printf("test and test3 are the same.\n");
 }
